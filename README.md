58681a154ff37d88396feb7b1163222c4f45e0d2
Rafael Machado <70187787+Rafael-Machado01@users.noreply.github.com>
2023-02-21 12:54:29 -0300

    comit init

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Folder Structure
-  For more complex projects
+
+For more complex projects
+
+src
+├───data
+│ ├───@types
+│ ├───contexts
+│ ├───hooks
+│ ├───reduces
+│ └───services
+├───pages
+│ └───home
+└───ui
+├───components
+├───partials
+├───styles
+└───theme
---
+++ source/pages/home/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "../ui/components/App.jsx";
+import App from "source/ui/components/App.jsx";
+source / pages / home / main.jsx;
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
